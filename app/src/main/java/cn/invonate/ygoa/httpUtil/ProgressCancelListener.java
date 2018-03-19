96c6b172a41e05abbffcc73355e470694c4c11ba
Acelyy <1084664238@qq.com>
2018-03-19 12:13:01 +0800

    日常更新

--- app/src/main/java/cn/invonate/ygoa/httpUtil/ProgressCancelListener.java
+++
@@ -1,8 +0,0 @@
-package cn.invonate.ygoa.httpUtil;
-
-/**
- * Created by liyangyang on 17/4/10.
- */
-public interface ProgressCancelListener {
-    void onCancelProgress();
-}
--- app/src/main/java/cn/invonate/ygoa/httpUtil/SubscriberOnNextListener.java
+++
@@ -1,8 +0,0 @@
-package cn.invonate.ygoa.httpUtil;
-
-/**
- * Created by liyangyang on 17/4/10.
- */
-public interface SubscriberOnNextListener<T> {
-    void onNext(T t);
-}
